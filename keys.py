36741f7f2ff897e95d35b5530b3cd734015766c7
Alejandro Hinojosa Rubio <hinojosa.rub@gmail.com>
2020-05-23 21:11:53 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,7 +2,7 @@
 # This file is not meant to be used in the main script.
 # Instead, copy this file into keys.py and put your own
 # keys there.
-CONSUMER_KEY = 'zp6bJ0mn4AQNxQJJu4TLeebaP'
-CONSUMER_SECRET = 'CBJvxCaKSFfMA2KYPx9iCzbSUO2kEQeGjp05c0tnNK36gFHVBX'
-ACCESS_KEY = '1263867556928069634-y9UNGLN39QOwcxmja7RX8gN2m7iLvG'
-ACCESS_SECRET = 'jyktvreTNCeaXCkPNaTOybjoNZi001a57wD0SQt7X6GBN'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
