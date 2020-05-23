7aafb423de0101710fd863235af3e61881ca9eac
Alejandro Hinojosa Rubio <hinojosa.rub@gmail.com>
2020-05-23 21:11:10 +0200

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,8 @@
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'zp6bJ0mn4AQNxQJJu4TLeebaP'
+CONSUMER_SECRET = 'CBJvxCaKSFfMA2KYPx9iCzbSUO2kEQeGjp05c0tnNK36gFHVBX'
+ACCESS_KEY = '1263867556928069634-y9UNGLN39QOwcxmja7RX8gN2m7iLvG'
+ACCESS_SECRET = 'jyktvreTNCeaXCkPNaTOybjoNZi001a57wD0SQt7X6GBN'
